ca0d6d5612d0fa19b69113b54c9db20f0536f1d5
taeshik <xmflr28@gmail.com>
2024-01-18 21:19:02 +0900

    test8

--- stepper-motor-test1.py
+++ stepper-motor-test1.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-EN = 15
 # Direction pin from controller
 DIR = 11
 # Step pin from controller
@@ -14,32 +13,44 @@ CCW = 0
 GPIO.setmode(GPIO.BOARD)
 
 # Establish Pins in software
-GPIO.setup(EN, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 
 # Set the first direction you want it to spin
-GPIO.output(EN, GPIO.HIGH)
-GPIO.output(DIR, GPIO.HIGH)
+GPIO.output(DIR, CW)
 
 try:
-    # Run forever.
-    while True:
-        print("start")
-        """Change Direction: Changing direction requires time to switch. The
-        time is dictated by the stepper motor and controller. """
-        # Run for 200 steps. This will change based on how you set your controller
-        for x in range(200):
-            # Set one coil winding to high
-            GPIO.output(STEP, GPIO.HIGH)
-            # Allow it to get there.
-            sleep(0.005)  # Dictates how fast stepper motor will run
-            # Set coil winding to low
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(0.005)  # Dictates how fast stepper motor will run
-        print("restart")
+	# Run forever.
+	while True:
+
+		"""Change Direction: Changing direction requires time to switch. The
+		time is dictated by the stepper motor and controller. """
+		sleep(1.0)
+		# Esablish the direction you want to go
+		GPIO.output(DIR,CW)
+
+		# Run for 200 steps. This will change based on how you set you controller
+		for x in range(200):
+
+			# Set one coil winding to high
+			GPIO.output(STEP,GPIO.HIGH)
+			# Allow it to get there.
+			sleep(.005) # Dictates how fast stepper motor will run
+			# Set coil winding to low
+			GPIO.output(STEP,GPIO.LOW)
+			sleep(.005) # Dictates how fast stepper motor will run
+
+		"""Change Direction: Changing direction requires time to switch. The
+		time is dictated by the stepper motor and controller. """
+		sleep(1.0)
+		GPIO.output(DIR,CCW)
+		for x in range(200):
+			GPIO.output(STEP,GPIO.HIGH)
+			sleep(.005)
+			GPIO.output(STEP,GPIO.LOW)
+			sleep(.005)
 
 # Once finished clean everything up
 except KeyboardInterrupt:
-    print("cleanup")
-    GPIO.cleanup()
+	print("cleanup")
+	GPIO.cleanup()
