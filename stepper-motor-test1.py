e78e7659484d443f6187ab61f3a3fe6f7b109d5e
taeshik <xmflr28@gmail.com>
2024-01-18 20:43:39 +0900

    test3

--- stepper-motor-test1.py
+++ stepper-motor-test1.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+EN = 9
 # Direction pin from controller
 DIR = 11
 # Step pin from controller
@@ -9,15 +10,18 @@ STEP = 13
 CW = 1
 CCW = 0
 
+
 # Setup pin layout on PI
 GPIO.setmode(GPIO.BOARD)
 
 # Establish Pins in software
+GPIO.setup(EN, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 
 # Set the first direction you want it to spin
-GPIO.output(DIR, CW)
+GPIO.output(EN, GPIO.LOW)
+GPIO.output(DIR, GPIO.LOW)
 
 try:
     # Run forever.
@@ -26,11 +30,7 @@ try:
         for x in range(200):
             # Set one coil winding to high
             GPIO.output(STEP, GPIO.HIGH)
-            # Allow it to get there.
-            sleep(0.005)  # Dictates how fast stepper motor will run
-            # Set coil winding to low
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(0.005)  # Dictates how fast stepper motor will run
+           
 
 # Once finished clean everything up
 except KeyboardInterrupt:
