89190a7619690dbda9d9cd4ef642dc6d82fdbf30
taeshik <xmflr28@gmail.com>
2024-02-15 21:37:08 +0900

    02-15_v1

--- Motor_Control.cpp
+++ Motor_Control.cpp
@@ -2,14 +2,14 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 
-#define	ENA	3	//ENA
-#define	DIR	0	//DIR
-#define	PUL	2	//PUL
+#define	ENA	15	//ENA
+#define	DIR	16	//DIR
+#define	PUL	1	//PUL
 
 using namespace std;
 
 void setup(){
-	wiringPiSetupGPIO();
+	wiringPiSetup();
   	pinMode(ENA, OUTPUT);
 	pinMode(DIR, OUTPUT);
 	pinMode(PUL, OUTPUT);
@@ -21,32 +21,30 @@ void setup(){
 
 int main(){
 	setup();
-	digitalWrite(ENA , HIGH);
-	cout << "ENA HIGH" << endl;
-	delay(2);
-	digitalWrite(ENA , LOW);
-	cout << "ENA LOW" << endl;
 	cout << "wiring pi" << endl;
 
 	while(1){
 		for(int i = 0; i < 1000; i++){
-		    digitalWrite(DIR, LOW);
+		        digitalWrite(DIR, LOW);
+		        digitalWrite(ENA, LOW);
 			digitalWrite(PUL, HIGH);
 			delay(1);
 			digitalWrite(PUL, LOW);
 			delay(1);
 		}
 
-		cout << "CHANGE DIR" << endl;
-		
-	    for(int i = 0; i < 1000; i++){
-	            digitalWrite(DIR, HIGH);
-	            digitalWrite(PUL, HIGH);
-	            delay(1);
-	            digitalWrite(PUL, LOW);
-       	        delay(1);
-        }
+	        for(int i = 0; i < 1000; i++){
+	                digitalWrite(DIR, HIGH);
+	                digitalWrite(ENA, LOW);
+	                digitalWrite(PUL, HIGH);
+	                delay(1);
+	                digitalWrite(PUL, LOW);
+       		        delay(1);
+        	}
 	}
+
+        digitalWrite(ENA, LOW);
+        digitalWrite(DIR, LOW);
 
 	return 0;
 }
